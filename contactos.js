b1e29ea2a0b456887bbc5bef01a87ac6ba515fb5
Nicolas <nicolaspulido1979@gmail.com>
2023-08-20 20:08:40 -0600

    actualizar

--- contactos.js
+++ contactos.js
@@ -42,10 +42,46 @@ function borrarContactoPorId(id) {
 }
 
 
+function actualizarContacto(idContacto, nuevoNombre,nuevoApellido, nuevoTelefono, nuevaCiudad,nuevaDirecion) {
+  for (let i = 0; i < listaDeContactos.length; i++) {
+      if (listaDeContactos[i].id === idContacto) {
+          if (nuevoNombre !== '') {
+              listaDeContactos[i].nombre = nuevoNombre;
+          }
+          if (nuevoApellido !== ''){
+            listaDeContactos[i].apellido = nuevoApellido;
+          }
+          if (nuevoTelefono !== '') {
+              listaDeContactos[i].telefono = nuevoTelefono;
+          }
+          if (nuevaCiudad !== '') {
+              listaDeContactos[i].ubicacion.ciudad = nuevaCiudad;
+          }
+          if (nuevaDirecion !== '') {
+            listaDeContactos[i].ubicacion.direccion = nuevaDirecion;
+        }
+          
+      }
+  }
+  
+}
+
 agregarContacto("Nicolas","pulido",3148566405,"facatativa-transversal 2 sur 1 a66")
 agregarContacto("Luisiño","Rafael",3121728328,"Venezuela-caracas")
 agregarContacto("Nicolas","pulido",3148566405,"facatativa","transversal 2 sur 1 a66")
+
+
 console.log("Lista antes de borrar:", listaDeContactos);
 
 borrarContactoPorId(2)
 console.log("Lista después de borrar:", listaDeContactos);
+
+
+
+actualizarContacto(1,"Felipe","gonzalo",312312312,"cartagena","transversal 1221")
+
+console.log("Lista despues de actualizar ",listaDeContactos); 
+
+
+
+
